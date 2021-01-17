330f46e4da7e3c2c3b788b147deb5d1702168bbf
lea-ben <leabenillouche@MacBook-Pro-de-Lea.local>
2021-01-17 17:09:49 +0200

    first commit

---
+++ week2/day1/dailychallenge/script.js
@@ -0,0 +1,25 @@
+let fruits = ["Banana", "Apples", "Oranges", "Blueberries"];
+
+fruits.shift ();
+
+console.log(fruits);
+
+fruits.sort (); 
+
+console.log (fruits);
+
+fruits.push ("kiwi");
+
+console.log (fruits);
+
+var removed = fruits.splice (1);
+
+removed.reverse (); 
+
+console.log(removed);
+
+var moreFruits = ["Banana", ["Apples", ["Oranges"], "Blueberries"]];
+
+console.log (moreFruits);
+
+console.log (moreFruits[3][0]);
---
+++ week2/day1/ex1.js
@@ -0,0 +1,11 @@
+var one= Number(prompt ("yo give me a number"));
+
+console.log (one);
+
+var second = parseInt(prompt ("another one pliz"));
+
+console.log(second); 
+
+var sum = (one + second) 
+
+alert (sum);
